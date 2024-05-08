1829f56987a63a48cfdf625c76d760a3bba93cd5
Matt_dev_on <127757483+Matt971x@users.noreply.github.com>
2024-05-08 18:39:33 +0200

    Add files via upload
    Final result

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const submitButton = document.querySelector("#btn-submit");
 const wrapperSuccess = document.querySelector(".wrapper-signUp-success");
 const wrapperSignUp = document.querySelector(".wrapper-signUp");
 const dissmissBtn = document.querySelector(".dissmiss-message");
+const span_email_user = document.querySelector(".email_user");
 
 const emailChecker = (value) => {
   if (!value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
@@ -10,25 +11,48 @@ const emailChecker = (value) => {
     return true;
   }
 };
-let emailAddress = "";
+const showEmailError = () => {
+  error.style.opacity = 1;
+  email.style.border = "1px solid  hsla(4, 100%, 67%)";
+  email.style.background = "hsla(4, 100%, 67%, 0.379)";
+};
+const hideEmailError = () => {
+  error.style.opacity = 0;
+  email.style.border = "";
+  email.style.background = "";
+};
+
+const showConfirmPage = () => {
+  wrapperSignUp.style.display = "none";
+  wrapperSuccess.style.display = "block";
+  span_email_user.textContent = email_user;
+};
+
+const hideConfirmPage = () => {
+  wrapperSuccess.style.display = "none";
+  wrapperSignUp.style.display = "flex";
+};
+
+let email_user = "";
 
 email.addEventListener("input", (e) => {
-  emailAddress = e.target.value;
+  email_user = e.target.value;
+  if (!emailChecker(email_user)) {
+    showEmailError();
+  } else {
+    hideEmailError();
+  }
 });
 
-submitButton.addEventListener("click", () => {
-  if (!emailChecker(emailAddress)) {
-    error.style.opacity = 1;
-    email.style.border = "1px solid  hsla(4, 100%, 67%)";
-    email.style.background = "hsla(4, 100%, 67%, 0.379)";
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!emailChecker(email_user)) {
+    showEmailError();
   } else {
-    wrapperSignUp.style.display = "none";
-    wrapperSuccess.style.display = "block";
-    mailSpan.textContent = emailAddress;
+    showConfirmPage();
   }
 });
 
 dissmissBtn.addEventListener("click", () => {
-  wrapperSuccess.style.display = "none";
-  wrapperSignUp.style.display = "flex";
+  hideConfirmPage();
 });
