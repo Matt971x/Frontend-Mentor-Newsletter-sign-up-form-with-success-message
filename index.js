fff5e2858f9e2da24ac1b97335c229191d9232f4
Matt_dev_on <127757483+Matt971x@users.noreply.github.com>
2024-05-08 10:33:15 +0200

    Add files via upload
    updated JS

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const submitButton = document.querySelector("#btn-submit");
 const wrapperSuccess = document.querySelector(".wrapper-signUp-success");
 const wrapperSignUp = document.querySelector(".wrapper-signUp");
 const dissmissBtn = document.querySelector(".dissmiss-message");
-const mailSpan = document.querySelector(".emailAddress");
 
 const emailChecker = (value) => {
   if (!value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
@@ -11,28 +10,17 @@ const emailChecker = (value) => {
     return true;
   }
 };
-
-let isValid;
 let emailAddress = "";
 
 email.addEventListener("input", (e) => {
-  if (!emailChecker(e.target.value)) {
+  emailAddress = e.target.value;
+});
+
+submitButton.addEventListener("click", () => {
+  if (!emailChecker(emailAddress)) {
     error.style.opacity = 1;
     email.style.border = "1px solid  hsla(4, 100%, 67%)";
     email.style.background = "hsla(4, 100%, 67%, 0.379)";
-    isValid = false;
-  } else {
-    error.style.opacity = 0;
-    email.style.border = "1px solid  lightgray";
-    email.style.background = "white";
-    isValid = true;
-    emailAddress = e.target.value;
-  }
-});
-
-submitButton.addEventListener("click", (e) => {
-  if (!isValid) {
-    error.style.opacity = 1;
   } else {
     wrapperSignUp.style.display = "none";
     wrapperSuccess.style.display = "block";
